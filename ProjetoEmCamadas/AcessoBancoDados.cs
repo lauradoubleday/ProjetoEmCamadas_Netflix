f3f530ee693bc288c8693d131334178d9214754e
Laura Doubleday <80928970+lauradoubleday@users.noreply.github.com>
2021-11-12 17:09:55 -0300

    Add files via upload

---
+++ ProjetoEmCamadas/AcessoBancoDados.cs
@@ -0,0 +1,69 @@
+﻿using MySqlConnector;
+using System;
+using System.Data;
+
+namespace ProjetoEmCamadas.Code.DAL
+{
+    class AcessoBancoDados
+    {
+        MySqlConnection conexao;
+
+        public void Conectar()
+        {
+            try
+            {
+                string conn = "Persist Security Info = false; " +
+                              "server = localhost; " +
+                              "database = bdprojetoemcamadas; " +
+                              "uid = root; pwd=root";
+
+                conexao = new MySqlConnection(conn);
+                conexao.Open();
+            }
+            catch (MySqlException ex)
+            {
+                throw new Exception("Não foi possível conectar ao banco de dados.\n" + ex.Message);
+            }
+        }
+
+        public DataTable ExecutarConsulta(string sql)       
+        {
+            try
+            {
+                Conectar();
+                MySqlDataAdapter dados = new MySqlDataAdapter(sql, conexao);
+                DataTable dt = new DataTable();
+                dados.Fill(dt);   
+
+                return dt;
+            }
+            catch (MySqlException ex)
+            {
+                throw new Exception("Não foi possível executar a CONSULTA solicitada.\n" + ex.Message);
+            }
+            finally
+            {
+                conexao.Close();
+            }
+        }
+
+        public void ExecutarComando(string sql)     
+        {
+            Conectar();
+
+            try
+            {
+                MySqlCommand comando = new MySqlCommand(sql, conexao);
+                comando.ExecuteNonQuery();     
+            }
+            catch (MySqlException ex)
+            {
+                throw new Exception("A instrução não foi realizada.\n" + ex.Message);
+            }
+            finally
+            {
+                conexao.Close();        
+            }
+        }
+    }
+}
---
+++ ProjetoEmCamadas/ProjetoEmCamadas/Frm_Netflix.cs
@@ -0,0 +1,81 @@
+﻿using ProjetoEmCamadas.Code.BLL;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProjetoEmCamadas
+{
+    public partial class Frm_Netflix : Form
+    {
+
+        NetflixBLL medbll = new NetflixBLL();
+        NetflixDTO meddto = new NetflixDTO();
+
+        public Frm_Netflix()
+
+        {
+            InitializeComponent();
+        }
+
+        private void btn_Cadastrar_Click(object sender, EventArgs e)
+        {
+            meddto.Filmes = txtFilmes.Text;
+            meddto.Produtor = txtProdutor.Text;
+            medbll.Inserir(meddto);
+
+            MessageBox.Show("Cadastrado com sucesso!","Netflix", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+            txtLogin.Clear();
+            txtFilmes.Clear();
+            txtProdutor.Clear();
+
+        }
+
+
+        private void btn_Excluir_Click(object sender, EventArgs e)
+        {
+            meddto.Login = int.Parse(txtLogin.Text);
+
+            medbll.Excluir(meddto);
+
+            MessageBox.Show("Excluído com sucesso!", "Filmes", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+            medbll.Listar();
+
+            txtLogin.Clear();
+            txtFilmes.Clear();
+            txtProdutor.Clear();
+        }
+
+        private void dgvNetflix_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            txtLogin.Text = dgvNetflix.Rows[e.RowIndex].Cells[0].Value.ToString();
+            txtFilmes.Text = dgvNetflix.Rows[e.RowIndex].Cells[1].Value.ToString();
+            txtProdutor.Text = dgvNetflix.Rows[e.RowIndex].Cells[2].Value.ToString();
+        }
+
+        private void btnEditar_Click(object sender, EventArgs e)
+        {
+            meddto.Login = int.Parse(txtLogin.Text);
+            meddto.Filmes = txtFilmes.Text;
+            meddto.Produtor = txtProdutor.Text;
+
+            medbll.Editar(meddto);
+
+            MessageBox.Show("Alterado com sucesso!", "Filmes", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+            medbll.Listar();
+
+            txtLogin.Clear();
+            txtFilmes.Clear();
+            txtProdutor.Clear();
+        }
+    }
+}
+   
